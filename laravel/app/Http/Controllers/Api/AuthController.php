e4960b916a66680d2f6c09c93d1ff16ae1624cec
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-09 13:38:14 +0000

    added storeUserRequest

---
+++ laravel/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register (StoreUserRequest $request)
+    {
+        
+    }
+}
