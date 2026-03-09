3ec07d688b89b2f60249ab8d7c2bcbe17fb7b5f5
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-09 12:03:10 +0000

    added stations table

---
+++ laravel/database/migrations/2026_03_09_115345_create_stations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('location');
+            $table->integer('power_kw');
+            $table->boolean('is_available')->default(true);
+
+            $table->foreignId('connector_type_id')->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stations');
+    }
+};
