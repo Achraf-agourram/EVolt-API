50beb51bcbf803ceda8e44a5cc7f5e26214a6261
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-09 13:30:39 +0000

    added connector model with relations

---
+++ laravel/app/Models/ConnectorType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ConnectorType extends Model
+{
+    protected $fillable = ['name'];
+
+    public function stations()
+    {
+        return $this->hasMany(Station::class);
+    }
+}
