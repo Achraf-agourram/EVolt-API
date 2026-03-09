fe591852a600b7a69aca6345bfd1ee545a27ee30
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-09 13:30:14 +0000

    added reservation model with relations

---
+++ laravel/app/Models/Reservation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'station_id',
+        'start_time',
+        'end_time',
+        'energy_kwh',
+        'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function station()
+    {
+        return $this->belongsTo(Station::class);
+    }
+}
