e7dcbf107ff91712961fd22959f000195908ab63
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-09 13:52:00 +0000

    added the register method

--- laravel/app/Http/Controllers/Api/AuthController.php
+++ laravel/app/Http/Controllers/Api/AuthController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function register (StoreUserRequest $request)
     {
-        
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+            'name' => $request->role,
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
     }
 }
