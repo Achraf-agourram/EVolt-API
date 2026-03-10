dc9b054b506253c000cf52c9ecc617d38df8c992
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-10 14:35:06 +0000

    feat: added reservationsPerStation energyPerStation

--- laravel/app/Http/Controllers/StatisticsController.php
+++ laravel/app/Http/Controllers/StatisticsController.php
@@ -14,6 +14,15 @@ class StatisticsController extends Controller
 
         $totalReservations = Reservation::count();
 
-        
+        $reservationsPerStation = Reservation::select('station_id')->selectRaw('COUNT(*) as total_reservations')->groupBy('station_id')->get();
+
+        $energyPerStation = Reservation::select('station_id')->selectRaw('SUM(energy_kwh) as total_energy')->where('status', 'completed')->groupBy('station_id')->get();
+
+        return response()->json([
+            'total_energy_delivered' => $totalEnergy,
+            'total_reservations' => $totalReservations,
+            'reservations_per_station' => $reservationsPerStation,
+            'energy_per_station' => $energyPerStation
+        ]);
     }
 }
