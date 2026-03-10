f35d15dc68bdd53b54118aa9894dbba2a5535390
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-10 11:16:54 +0000

    feat: added check statement before booking a station to avoid conflicts

--- laravel/app/Http/Controllers/ReservationController.php
+++ laravel/app/Http/Controllers/ReservationController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    public function book ()
+    public function book (Request $request)
     {
-        //
+        $checkReservedStation = Reservation::where('station_id', $request->station_id)->where('status', '!=', 'cancelled')->whereBetween('start_time', [$request->start_time, $request->end_time])->orWhereBetween('end_time', [$request->start_time, $request->end_time]);
+
+        if ($checkReservedStation) return response()->json(['message' => 'This station is already reserved for this time period'], 403);
+
+        
     }
 }
