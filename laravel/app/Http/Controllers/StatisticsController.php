3ca26cf521e6a44d5deb2774446ae57b8934307c
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-10 14:33:43 +0000

    feat: added totalEnergy and totalReservations

--- laravel/app/Http/Controllers/StatisticsController.php
+++ laravel/app/Http/Controllers/StatisticsController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StatisticsController extends Controller
 {
-    //
+    public function index()
+    {
+        $totalEnergy = Reservation::where('status', 'completed')->sum('energy_kwh');
+
+        $totalReservations = Reservation::count();
+
+        
+    }
 }
