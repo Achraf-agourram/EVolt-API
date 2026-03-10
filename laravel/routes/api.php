f7589ad2ae20719088fc5132788312b029b6f2dc
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-10 09:44:20 +0000

    feat: created stationController

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\StationController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -9,6 +10,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/seach', [StationController::class, 'search']);
 
 });
 
